0a374100b11b40ee7690d4cf001e2b0e3447bf0d
Kgabo-Duncan <kd.thantsha@gmail.com>
2024-05-02 16:19:58 +0200

    Lab1 Solutuions

---
+++ Lab01.sql
@@ -0,0 +1,5 @@
+/*1. Retrieve customer details
+Familiarize yourself with the Customer table by writing a Transact-SQL query that retrieves all columns 
+for all customers.*/SELECT TOP 10 * 	FROM [SalesLT].[Customer];/*2. Retrieve customer name data
+Create a list of all customer contact names that includes the title, first name, middle name (if any), last 
+name, and suffix (if any) of all customers.*/
