49c8a491771c00c42e9a0c8a8d9517db308ba955
Kgabo-Duncan <kd.thantsha@gmail.com>
2024-05-09 21:31:10 +0200

    lab4 last commmmit

--- Lab04.sql
+++ Lab04.sql
@@ -26,11 +26,11 @@ SalesLT.CustomerAddress table is ‘Main Office’.*/
 
 SELECT c.[CompanyName], a.[AddressLine1], a.[City], 'Billing' AS [AddressType] 
 FROM [SalesLT].[Customer] AS c
-	JOIN [SalesLT].[CustomerAddress] AS ca 
-ON c.[CustomerID] = ca.[CustomerID]
-	JOIN [SalesLT].[Address] AS a 
-ON a.[AddressID] = ca.[AddressID]
-WHERE ca.[AddressType] = 'Main Office';
+	INNER JOIN [SalesLT].[CustomerAddress] AS ca 
+ON c.[CustomerID] = ca.[CustomerID]-- join based on CustomerID
+	INNER JOIN [SalesLT].[Address] AS a 
+ON a.[AddressID] = ca.[AddressID]----join based on AddressID
+WHERE ca.[AddressType] = 'Main Office';-- filter the results where AddressType is main office
 
 /*2. Retrieve shipping addresses
 Write a similar query that retrieves the company name, first line of the street address, city, and a 
@@ -39,9 +39,9 @@ SalesLT.CustomerAddress table is ‘Shipping’.*/
 
 SELECT c.[CompanyName], a.[AddressLine1], a.[City], 'Shipping' as [AddressType]
 FROM  [SalesLT].[Customer] AS c
-	JOIN [SalesLT].[CustomerAddress] AS ca 
+	INNER JOIN [SalesLT].[CustomerAddress] AS ca 
 ON c.[CustomerID] = ca.[CustomerID]
-	JOIN [SalesLT].[Address] AS a
+	INNER JOIN [SalesLT].[Address] AS a
 ON ca.[AddressID] = A.[AddressID]
 WHERE ca.[AddressType] ='Shipping';
 
@@ -52,17 +52,17 @@ by company name and then address type.*/
 
 SELECT c.[CompanyName], a.[AddressLine1], a.[City], 'Billing' AS [AddressType] 
 FROM [SalesLT].[Customer] AS c
-	JOIN [SalesLT].[CustomerAddress] AS ca 
+	INNER JOIN [SalesLT].[CustomerAddress] AS ca 
 ON c.[CustomerID] = ca.[CustomerID]
-	JOIN [SalesLT].[Address] AS a 
+	INNER JOIN [SalesLT].[Address] AS a 
 ON a.[AddressID] = ca.[AddressID]
 WHERE ca.[AddressType] = 'Main Office'
 UNION ALL
 SELECT c.[CompanyName], a.[AddressLine1], a.[City], 'Shipping' as [AddressType]
 FROM  [SalesLT].[Customer] AS c
-	JOIN [SalesLT].[CustomerAddress] AS ca 
+	INNER JOIN [SalesLT].[CustomerAddress] AS ca 
 ON c.[CustomerID] = ca.[CustomerID]
-	JOIN [SalesLT].[Address] AS a
+	INNER JOIN [SalesLT].[Address] AS a
 ON ca.[AddressID] = a.[AddressID]
 WHERE ca.[AddressType] ='Shipping'
 ORDER BY c.[CompanyName], [AddressType];
@@ -72,14 +72,51 @@ ORDER BY c.[CompanyName], [AddressType];
 You have created a master list of all customer addresses, but now you have been asked to create filtered 
 lists that show which customers have only a main office address, and which customers have both a main 
 office and a shipping address.*/
-Tip: Review the documentation for the EXCEPT and INTERSECT operators in the Transact-SQL Reference.
-1. Retrieve customers with only a main office address
+--Tip: Review the documentation for the EXCEPT and INTERSECT operators in the Transact-SQL Reference.
+
+/*1. Retrieve customers with only a main office address
 Write a query that returns the company name of each company that appears in a table of customers 
-with a ‘Main Office’ address, but not in a table of customers with a ‘Shipping’ address.
-2. Retrieve only customers with both a main office address and a shipping address
+with a ‘Main Office’ address, but not in a table of customers with a ‘Shipping’ address.*/
+
+SELECT c.[CompanyName]
+FROM [SalesLT].[Customer] AS c
+	INNER JOIN [SalesLT].[CustomerAddress] AS ca 
+ON c.[CustomerID] = ca.[CustomerID]
+	INNER JOIN [SalesLT].[Address] AS a 
+ON a.[AddressID] = ca.[AddressID]
+WHERE ca.[AddressType] = 'Main Office'
+EXCEPT
+SELECT c.[CompanyName]
+FROM  [SalesLT].[Customer] AS c
+	INNER JOIN [SalesLT].[CustomerAddress] AS ca 
+ON c.[CustomerID] = ca.[CustomerID]
+	INNER JOIN [SalesLT].[Address] AS a
+ON ca.[AddressID] = a.[AddressID]
+WHERE ca.[AddressType] ='Shipping'
+ORDER BY c.[CompanyName];
+
+/*2. Retrieve only customers with both a main office address and a shipping address
 Write a query that returns the company name of each company that appears in a table of customers 
-with a ‘Main Office’ address, and also in a table of customers with a ‘Shipping’ address.
-Next Steps
+with a ‘Main Office’ address, and also in a table of customers with a ‘Shipping’ address.*/
+
+SELECT c.[CompanyName]
+FROM [SalesLT].[Customer] AS c
+	INNER JOIN [SalesLT].[CustomerAddress] AS ca 
+ON c.[CustomerID] = ca.[CustomerID]
+	INNER JOIN [SalesLT].[Address] AS a 
+ON a.[AddressID] = ca.[AddressID]
+WHERE ca.[AddressType] = 'Main Office'
+INTERSECT
+SELECT c.[CompanyName]
+FROM  [SalesLT].[Customer] AS c
+	INNER JOIN [SalesLT].[CustomerAddress] AS ca 
+ON c.[CustomerID] = ca.[CustomerID]
+	INNER JOIN [SalesLT].[Address] AS a
+ON ca.[AddressID] = a.[AddressID]
+WHERE ca.[AddressType] ='Shipping'
+ORDER BY c.[CompanyName];
+
+/*Next Steps
 Well done! You’ve completed the lab, and you’re ready to learn how to add aggregate functions and 
 groupings to your queries in Module 5 – Using Functions and Aggregating Data in the Course Querying 
-with Transact-SQL
+with Transact-SQL*/
