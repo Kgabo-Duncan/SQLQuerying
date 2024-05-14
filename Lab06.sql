20312084bbe4dbb4af46b9b106c7be1fa381f2eb
Kgabo-Duncan <kd.thantsha@gmail.com>
2024-05-14 12:03:36 +0200

    Modified Lab6 file

--- Lab06.sql
+++ Lab06.sql
@@ -1,4 +1,4 @@
-﻿Querying with Transact-SQL
+﻿/*Querying with Transact-SQL
 Lab 6 – Using Subqueries and APPLY
 Overview
 In this lab, you will use subqueries and the APPLY operator to retrieve data from the AdventureWorksLT
@@ -17,32 +17,83 @@ product, and a list price that indicates the recommended selling price for the p
 in the SalesLT.Product table. Whenever a product is ordered, the actual unit price at which it was sold is 
 also recorded in the SalesLT.SalesOrderDetail table. You must use subqueries to compare the cost and 
 list prices for each product with the unit prices charged in each sale.
-Tip: Review the documentation for subqueries in Subquery Fundamentals.
-1. Retrieve products whose list price is higher than the average unit price
+Tip: Review the documentation for subqueries in Subquery Fundamentals.*/
+
+/*1. Retrieve products whose list price is higher than the average unit price
 Retrieve the product ID, name, and list price for each product where the list price is higher than the 
-average unit price for all products that have been sold.
-2. Retrieve Products with a list price of $100 or more that have been sold for less than 
+average unit price for all products that have been sold.*/
+
+SELECT p.[ProductID], p.[Name], p.[ListPrice]
+	FROM [SalesLT].[Product] AS p
+WHERE p.[ListPrice] > (	SELECT AVG([UnitPrice]) 
+							FROM [SalesLT].[SalesOrderDetail] AS sod) 
+ORDER BY p.[ListPrice] DESC;
+
+/*2. Retrieve Products with a list price of $100 or more that have been sold for less than 
 $100
 Retrieve the product ID, name, and list price for each product where the list price is $100 or more, and 
-the product has been sold for less than $100.
-3. Retrieve the cost, list price, and average selling price for each product
+the product has been sold for less than $100.*/
+
+SELECT p.[ProductID], p.[Name], p.[ListPrice]
+		FROM [SalesLT].[Product] AS p
+	WHERE p.[ProductID] IN ( SELECT [ProductID] 
+								FROM [SalesLT].[SalesOrderDetail] AS sod
+							 WHERE p.[ListPrice] >= 100 AND sod.[UnitPrice] < 100)
+ORDER BY p.[ListPrice] DESC;
+
+/*3. Retrieve the cost, list price, and average selling price for each product
 Retrieve the product ID, name, cost, and list price for each product along with the average unit price for 
-which that product has been sold.
-4. Retrieve products that have an average selling price that is lower than the cost
+which that product has been sold.*/
+
+SELECT p.[ProductID], p.[Name], p.[StandardCost], p.[ListPrice],
+	  (	SELECT AVG(UnitPrice)
+			FROM [SalesLT].[SalesOrderDetail] AS sod
+		WHERE p.[ProductID] = sod.[ProductID]) AS AvgSellingPrice
+FROM [SalesLT].[Product] AS p 
+ORDER BY p.[ProductID];
+
+/*4. Retrieve products that have an average selling price that is lower than the cost
 Filter your previous query to include only products where the cost price is higher than the average 
-selling price.
-Challenge 2: Retrieve Customer Information
+selling price.*/
+SELECT p.[ProductID], p.[Name], p.[StandardCost], p.[ListPrice],
+	  ( SELECT AVG(UnitPrice)
+			FROM [SalesLT].[SalesOrderDetail] AS sod
+		WHERE p.[ProductID] = sod.[ProductID] ) AS AvgSellingPrice
+	FROM [SalesLT].[Product] AS p 
+WHERE p.[StandardCost] >
+    ( SELECT AVG([UnitPrice])
+		FROM [SalesLT].[SalesOrderDetail] AS sod
+	  WHERE p.[ProductID] = sod.[ProductID])
+ORDER BY p.[ProductID];
+
+/*Challenge 2: Retrieve Customer Information
 The AdventureWorksLT database includes a table-valued user-defined function named 
 dbo.ufnGetCustomerInformation. You must use this function to retrieve details of customers based on 
 customer ID values retrieved from tables in the database.
-Tip: Review the documentation for the APPLY operator in Using APPLY.
-1. Retrieve customer information for all sales orders
+Tip: Review the documentation for the APPLY operator in Using APPLY.*/
+
+/*1. Retrieve customer information for all sales orders
 Retrieve the sales order ID, customer ID, first name, last name, and total due for all sales orders from 
-the SalesLT.SalesOrderHeader table and the dbo.ufnGetCustomerInformation function.
-2. Retrieve customer address information
+the SalesLT.SalesOrderHeader table and the dbo.ufnGetCustomerInformation function.*/
+
+sp_helptext 'dbo.ufnGetCustomerInformation'
+
+SELECT soh.[SalesOrderID],soh.[CustomerID], soh.[TotalDue],ci.[FirstName], ci.[LastName]
+	FROM [SalesLT].[SalesOrderHeader] AS soh
+		CROSS APPLY dbo.ufnGetCustomerInformation(soh.[CustomerID]) AS ci
+ORDER BY soh.[CustomerID];
+
+/*2. Retrieve customer address information
 Retrieve the customer ID, first name, last name, address line 1 and city for all customers from the
 SalesLT.Address and SalesLT.CustomerAddress tables, and the dbo.ufnGetCustomerInformation
-function.
-Next Steps
+function.*/
+SELECT ca.[CustomerID], ci.[FirstName], ci.[LastName], a.[AddressLine1], a.[City]
+	FROM [SalesLT].[Address] AS a
+		INNER JOIN [SalesLT].[CustomerAddress] AS ca
+	ON a.[AddressID] = ca.[AddressID]
+		CROSS APPLY dbo.ufnGetCustomerInformation(ca.[CustomerID]) AS ci
+ORDER BY ca.[CustomerID];
+
+/*Next Steps
 Well done! You’ve completed the lab, and you’re ready to learn how to use table expressions in Module 
-7 – Using Table Expressions in the Course Querying with Transact-SQL
+7 – Using Table Expressions in the Course Querying with Transact-SQL*/
